9f8e618b8b1dfec24c5973b8a3482f29d23a54b7
heng-ning <maggie310254@gmail.com>
2022-02-22 18:42:53 +0800

    lab

--- JS/index.js
+++ JS/index.js
@@ -1,5 +1,54 @@
 
+//輪播
+$('.owl-carousel').owlCarousel({
+  loop:true,
+  margin:10,
+  nav:true,
+  responsive:{
+      0:{
+          items:1
+      },
+      600:{
+          items:3
+      },
+      1000:{
+          items:5
+      }
+  }
+})
+
+
 //menu
+Fancybox.bind('[data-fancybox="gallery"]', {
+  dragToClose: false,
+
+  Toolbar: false,
+  closeButton: "top",
+
+  Image: {
+    zoom: false,
+  },
+
+  on: {
+    initCarousel: (fancybox) => {
+      const slide = fancybox.Carousel.slides[fancybox.Carousel.page];
+
+      fancybox.$container.style.setProperty(
+        "--bg-image",
+        `url("${slide.$thumb.src}")`
+      );
+    },
+    "Carousel.change": (fancybox, carousel, to, from) => {
+      const slide = carousel.slides[to];
+
+      fancybox.$container.style.setProperty(
+        "--bg-image",
+        `url("${slide.$thumb.src}")`
+      );
+    },
+  },
+  });
+
 
 
 //News 詳情
@@ -15,22 +64,43 @@ function hidediv(){
 
 }
 
-(function () {
-  'use strict'
+// (function () {
+//   'use strict'
 
-  // Fetch all the forms we want to apply custom Bootstrap validation styles to
-  var forms = document.querySelectorAll('.needs-validation')
+//   // Fetch all the forms we want to apply custom Bootstrap validation styles to
+//   var forms = document.querySelectorAll('.needs-validation')
 
-  // Loop over them and prevent submission
-  Array.prototype.slice.call(forms)
-    .forEach(function (form) {
-      form.addEventListener('submit', function (event) {
-        if (!form.checkValidity()) {
-          event.preventDefault()
-          event.stopPropagation()
-        }
+//   // Loop over them and prevent submission
+//   Array.prototype.slice.call(forms)
+//     .forEach(function (form) {
+//       form.addEventListener('submit', function (event) {
+//         if (!form.checkValidity()) {
+//           event.preventDefault()
+//           event.stopPropagation()
+//         }
 
-        form.classList.add('was-validated')
-      }, false)
-    })
-})()
+//         form.classList.add('was-validated')
+//       }, false)
+//     })
+// })()
+
+
+
+// $(document).ready(function(){
+//   $(".btn1").click(function(){
+//   $(this).next(".card-text2").slideToggle();
+//   });
+// });
+var btn1 = document.getElementsByClassName("btn1");
+var i;
+for (i = 0; i < btn1.length; i++) {
+  btn1[i].addEventListener("click", function() {
+  this.classList.toggle("showcontent");
+  var showcontent = this.nextElementSibling;
+  if (showcontent.style.display === "block") {
+    showcontent.style.display = "none";
+  } else {
+    showcontent.style.display = "block";
+  }
+  });
+}
